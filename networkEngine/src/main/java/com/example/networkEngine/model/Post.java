730cbe9bc6cc16a53a06120524611ab01e133cd6
Pavel_Jake <likantropianebeda@gmail.com>
2021-11-01 05:17:17 +0300

    tags, user, posts added

--- networkEngine/src/main/java/com/example/networkEngine/model/Post.java
+++ networkEngine/src/main/java/com/example/networkEngine/model/Post.java
@@ -1,4 +1,25 @@
 package com.example.networkEngine.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "is_active")
+    private short isActive;
+    ///////////////////////////////////место для статуса модерации в енаме
+    @Column(name = "moderator_id")
+    private int moderatorId;//////////////переделать в модератора?
+    @Column(name = "user_id")
+    private int userId;//////////////переделать в юзера?
+    private Date time;
+    private String title;
+    private String text;
+    @Column(name = "view_count")
+    private int viewCount;
 }
